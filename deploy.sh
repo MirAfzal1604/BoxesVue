8e6adbdcafcc139ca794aa39bb970f856021108b
MirAfzal <92784299+MirAfzal1604@users.noreply.github.com>
2022-01-14 20:04:25 +0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:mirafzal1604/VueBox.git main:gh-pages
+git push -f git@github.com:mirafzal1604/BoxesVue.git main:gh-pages
 
 cd -
